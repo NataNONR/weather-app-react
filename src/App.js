9120d91df99754fb2cbf9898f7d03fddf5cb7bfc
Nata <anblurry@gmail.com>
2022-03-04 17:20:21 +0100

    Feat: Weather Data
    Add API integration and API call function to get the data

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="Paris" />
         <footer>Coded by Nattha Oroz</footer>
       </div>
     </div>
